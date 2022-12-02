1d8ded4e272d6ac0dfab2c7d0dc59cbe105017dc
kalpa-fire <danilosoares@gmail.com>
2022-12-02 14:20:46 +0000

    feat: adding cors for frontend usage

--- backend/app.ts
+++ backend/app.ts
@@ -1,10 +1,14 @@
 import express from 'express';
 import prometheusMiddleware from 'express-prometheus-middleware';
+import cors from 'cors';
 import TimeController from './controllers/time.controller';
 import auth from './middleware/auth';
 
 const app = express();
 
+app.use(cors({
+    origin: '*'
+}))
 app.use(auth);
 app.use(prometheusMiddleware({
     metricsPath: '/metrics',
